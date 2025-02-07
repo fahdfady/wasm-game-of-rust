2eb4a7beee327ee25f4cd9cd29bbd61b1fa2f902
fahdfady <fahd.fady212@gmail.com>
2025-02-07 19:38:03 +0200

    basic implementation of conway's game of life in rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,117 @@
 mod utils;
 
+use std::fmt;
+use std::fmt::Formatter as fmtFormatter;
+use std::fmt::Result as fmtResult;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
-static ALLOC: wee_aloloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// imports the `window.alert` javascript function
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[repr(u8)] // each cell is represented as a single byte.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
 
-// exports a `greet` rust function
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    let message = format!("Hello, {} ,wasm-game-of-rust!", name);
-    alert(message.as_str());
+// fixed-sized universe.
+// this makes infinite patterns, like gliders, that reach the end of the universe are snuffed out.
+struct Universe {
+    width: u32,  // 8*4
+    height: u32, // 8*4
+    cells: Vec<Cell>,
+}
+
+// formula to find the array index of the cell inside of the universe
+//  index(row, column, universe) = row * width of universe + column
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+
+    /// Public methods are exported to JavaScript.
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for row in 0..self.height {
+            for column in 0..self.width {
+                let index = self.get_index(row, column);
+                let cell = self.cells[index];
+
+                let live_neighbors = self.live_neighbor_count(row, column);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3 Any live cell with more than three live neighbours dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                next[index] = next_cell;
+            }
+        }
+    }
+
+    pub fn new() -> Universe {
+        let width: u32 = 64;
+        let height: u32 = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmtFormatter) -> fmtResult {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
